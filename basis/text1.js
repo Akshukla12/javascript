4dfdc9166df05a744d76331990447a1949dfd529
Akshukla12 <114152828+Akshukla12@users.noreply.github.com>
2024-07-28 10:19:30 +0000

    commit

---
+++ basis/text1.js
@@ -0,0 +1,23 @@
+
+/*constant and variable */
+
+
+const accountId =123123
+let accountEmail ="akashshukla2311@gmail.com"
+var accountPassword ="7767878"
+accountCity ="sitapur"
+
+
+accountEmail="ajsnndjajnc"
+accountPassword="111111"
+accountCity="lucknow"
+//accountId=23
+
+//console.log(accountId);
+console.table ([accountId,accountEmail,accountPassword,accountCity])
+
+/*
+--prefer not to use var becuase of issue in block scope and functional scope.
+--use const and let 
+--if variable is declared and value is not defined then outpur is undefined                           
+*/
---
+++ basis/text2.js
@@ -0,0 +1,22 @@
+/*data type*/
+"use strict"//treat all js code as newer version 
+//alert(3+3)  we are using nodejs,not browser 
+
+/*
+number=>2to power 53
+bigint
+string=>""
+boolean=>true/false
+null=>standalone value or means empty
+undefined=>value is not defined 
+symbol=>uniqueness
+
+
+object=>
+*/
+//datatype of null is object
+
+
+
+console.log(typeof "akash");// typeof used to find the data type of a variable
+let name="akash"
